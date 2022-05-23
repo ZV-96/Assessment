5c6846afae99145345cae25fb5714929140ec2b8
ZV-96 <98915389+ZV-96@users.noreply.github.com>
2022-05-23 12:40:12 +1200

    Create 04_questions_v2.py

---
+++ 04_questions_v2.py
@@ -0,0 +1,30 @@
+"""The second version of 03_questions
+differentiating between invalid answers and incorrect answers"""
+
+
+def questions():
+
+    error = "Invalid input"
+    score = 0
+    answer = input("are you ready to play?: ")
+    if answer.lower() == "yes" or "y":
+        answer = input("Question 1: What does Tahi mean in maori \n"
+                           "a. 1 \n"
+                           "b. 5 \n"
+                           "c. 10 \n")
+
+        while answer.lower() == "a":
+            print("correct answer")
+            score += 1
+            break
+
+        except ValueError:
+
+        else:
+            print("incorrect answer")
+
+
+# Main routine
+questions()
+print("next question continues")
+
