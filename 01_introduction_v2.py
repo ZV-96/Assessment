81db4ab12b1d2ee049cccd23c4590c8e0873d4c9
ZV-96 <98915389+ZV-96@users.noreply.github.com>
2022-05-26 09:08:28 +1200

    update

--- 01_introduction_v2.py
+++ 01_introduction_v2.py
@@ -1,20 +1,28 @@
 """01_introduction_v2
-this version is a improvement on 01_introduction_v1 """
+this version is an improvement on 01_introduction_v1 """
 
 
+# Functions
+# This function will get the players name
 def get_name():
     name = str(input("What is your name: "))
     return name
 
 
-def get_age(): # This function will get the players age
+# This function will get the players age
+def get_age():
     age = int(input("How old are you: "))
-    return age
+    if 1 <= age <= 100:
+        return age
+
+    else:
+        print("<Error> please enter a valid whole number")
+        return get_age()
 
 
 # Main routine
 name = get_name()
 age = get_age()
-print(formatter("-", f"Kia ora {name}. Welcome to Mamae Roro.\n"
-      f"Since you are {age} years old you will probably find this easy"))
+print(f"Kia ora {name}. Welcome to The maori quiz.\n"
+      f"Since you are {age} years old you will probably find this easy")
 
